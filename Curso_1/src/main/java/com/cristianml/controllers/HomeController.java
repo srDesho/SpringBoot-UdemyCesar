a2af89571b277461170b7ee93bfc80b175b7ee8f
Cristian Montaño Laime <cristianlai775@gmail.com>
2024-04-02 02:51:31 -0400

    Uso de @ResponseBody

--- Curso_1/src/main/java/com/cristianml/controllers/HomeController.java
+++ Curso_1/src/main/java/com/cristianml/controllers/HomeController.java
@@ -2,6 +2,7 @@ package com.cristianml.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 // Agregamos la anotación @Controller para especificar que éste es un controlador
 @Controller
@@ -10,8 +11,9 @@ public class HomeController {
 	// Creamos un método que va a cargar una ruta y le asignamos el @GetMapping("/") para que sepa que 
 	// cargará una ruta.
 	
-	@GetMapping("/")
+	@GetMapping("/") // Sólo slash devuelve algo vacío
+	@ResponseBody // Sirve para mostrar texto en la ejecución del navegador (casi no se usa en lo laboral)
 	public String home() {
-		return "";
+		return "Hola mundito";
 	}
 }
