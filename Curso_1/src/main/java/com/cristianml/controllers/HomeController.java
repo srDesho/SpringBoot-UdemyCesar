441ed4b6f65601e97f18d436e0437e583f39d9f0
Cristian Montaño Laime <cristianlai775@gmail.com>
2024-04-02 19:19:17 -0400

    Uso anotación @RequestMapping

--- Curso_1/src/main/java/com/cristianml/controllers/HomeController.java
+++ Curso_1/src/main/java/com/cristianml/controllers/HomeController.java
@@ -2,10 +2,12 @@ package com.cristianml.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 // Agregamos la anotación @Controller para especificar que éste es un controlador
 @Controller
+@RequestMapping("/") // Se pone debaje del @Controller y sirve para indicar una ruta, similar al @GetMapping
 public class HomeController {
 
 	// Creamos un método que va a cargar una ruta y le asignamos el @GetMapping("/") para que sepa que 
@@ -15,5 +17,11 @@ public class HomeController {
 	@ResponseBody // Sirve para mostrar texto en la ejecución del navegador (casi no se usa en lo laboral)
 	public String home() {
 		return "Hola mundito, más texto."; 
+	}
+	
+	@GetMapping("/nosotros")
+	@ResponseBody
+	public String nostros() {
+		return "Hola desde nosotros";
 	}
 }
