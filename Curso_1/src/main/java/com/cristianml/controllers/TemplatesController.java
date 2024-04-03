ef5fdcc5e5b99c8901d2942050a7503390e0a5fe
Cristian Montaño Laime <cristianlai775@gmail.com>
2024-04-03 18:33:40 -0400

    Pasando variables como parametros con la clase Model

--- Curso_1/src/main/java/com/cristianml/controllers/TemplatesController.java
+++ Curso_1/src/main/java/com/cristianml/controllers/TemplatesController.java
@@ -1,6 +1,7 @@
 package com.cristianml.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,7 +10,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class TemplatesController {
 	
 	@GetMapping("")
-	public String home() {
-		return "templates/home";
+	// Pasamos parámetros con una variable String para mostrar en el html con thymeleaf, pasamos con un model
+	public String home(Model model) {
+		// Creamos variables
+		String nombre = "Cristian Montaño Laime";
+		String pais = "Bolivia";
+		
+		// Agregamos al model con un nombre entre comillas el cual será usado para ser llamado desde html thymeleaf
+		model.addAttribute("nombre", nombre);
+		model.addAttribute("pais", pais);
+		return "templates/home.html";
 	}
 }
