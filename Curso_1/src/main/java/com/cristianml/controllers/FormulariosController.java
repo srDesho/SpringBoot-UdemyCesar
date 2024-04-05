cce24bf895c1b1433cf28d6cc3620b5a88b3ff38
Cristian Montaño Laime <cristianlai775@gmail.com>
2024-04-05 19:10:15 -0400

    Recibiendo datos de un formulario simple con @PostMapping

--- Curso_1/src/main/java/com/cristianml/controllers/FormulariosController.java
+++ Curso_1/src/main/java/com/cristianml/controllers/FormulariosController.java
@@ -2,7 +2,10 @@ package com.cristianml.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/formularios")
@@ -14,4 +17,20 @@ public class FormulariosController {
 	public String home() {
 		return("formularios/home");
 	}
+	
+	// ================================ Formulario Simple ==================================
+	@GetMapping("/simple")
+	public String simple() {
+		return "/formularios/simple";
+	}
+	
+	// Creamos el método con @PostMapping para recibir el formulario 
+	@PostMapping("/simple")
+	@ResponseBody
+	public String simple_post(@RequestParam("username") String username, @RequestParam("email") String email,
+			@RequestParam("password") String password) {
+		return "username= " + username + "<br>email= " + email + "<br>password= " + password;
+	}
+	
+	// ================================ Formulario De Objetos ==================================
 }
