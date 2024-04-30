3b818c4018892eb2c6b402025da57c8433811cac
Cristian Montaño Laime <cristianlai775@gmail.com>
2024-04-29 20:30:05 -0400

    Creacion de Proyecto del curso para bloque Spring Security

---
+++ Curso_2_spring_security/src/main/java/com/cristianml/Curso2SpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.cristianml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Curso2SpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Curso2SpringSecurityApplication.class, args);
+	}
+
+}
