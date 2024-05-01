a01207c86986356324c5b2a03cd127258d849152
Cristian Montaño Laime <cristianlai775@gmail.com>
2024-05-01 19:34:20 -0400

    Metodo AuthenticationManager para configuracion de s. security

--- Curso_2_spring_security/src/main/java/com/cristianml/seguridad/Seguridad.java
+++ Curso_2_spring_security/src/main/java/com/cristianml/seguridad/Seguridad.java
@@ -1,6 +1,9 @@
 package com.cristianml.seguridad;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
@@ -13,4 +16,17 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class Seguridad {
 
+	// Creamos el método que spring security utilizará para hacer todas las configuraciones, sobre todo para autenticación
+	
+	// La anotación @Bean en Spring Boot se utiliza para indicarle a Spring que un método específico en una clase de configuración 
+	// debe ser tratado como un método de inicialización para construir y configurar un objeto gestionado por Spring en el 
+	// contenedor de Spring (ApplicationContext).
+	// En resumen, @Bean se utiliza para definir métodos que producen beans administrados por Spring.
+	// Esto significa que Spring manejará el ciclo de vida y la configuración de estos objetos.
+	
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
+	}
+	
 }
