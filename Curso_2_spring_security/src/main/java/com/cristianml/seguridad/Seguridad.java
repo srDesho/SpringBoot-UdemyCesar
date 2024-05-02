837f757cf20b977b4e555fec1fe0d1ad53d4db4e
Cristian Montaño Laime <cristianlai775@gmail.com>
2024-05-02 18:48:58 -0400

    Configurando los accesos con SecurityFilterChain u authorizeRequest

--- Curso_2_spring_security/src/main/java/com/cristianml/seguridad/Seguridad.java
+++ Curso_2_spring_security/src/main/java/com/cristianml/seguridad/Seguridad.java
@@ -1,11 +1,15 @@
 package com.cristianml.seguridad;
 
+import javax.annotation.security.PermitAll;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 // Esta clase la vamos a usar para configurar el core del framework
 
@@ -29,4 +33,36 @@ public class Seguridad {
 		return authenticationConfiguration.getAuthenticationManager();
 	}
 	
+	// Creamos otro método para poder configurar nuestro filtros con SecurityFilterChain
+	// el paráetro http es el que vamos a configurar para poder realizar los filtros
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		// authorizeHttpRequests() nos permite hacer las configuraciones
+		http.authorizeHttpRequests()
+		
+		// las vistas que no van a tener protección, o sea que serán públicas para que el usuario pueda visualizar
+		.antMatchers(
+				"/",
+				"/liberado/**", // doble asterisco para que todo que esté adelante de la ruta liberada no tenga protección
+				"/acceso/registro"
+				).permitAll() // este método permite la configuración solo para lo que está dentro del paréntesis
+		
+		// asignar permisos a URLs por ROLES
+		.antMatchers(
+				"/protegido/**"
+				// Con este método le indicamos los roles que queremos que accesen a las rutas que escribimos
+				).hasAnyAuthority("ROLE_ADMIN", "ROLE_USER")
+		
+		// Ahora toca hacer las configuraciones generales
+		.anyRequest().authenticated() // para decirle al usuario que se va a autenticar con esto
+		
+		// Indicamos el formato del login
+		.and().formLogin().permitAll()
+		// Lo mismo hacemos con el logout
+		.and().logout().permitAll()
+				;
+		
+		// el .build() es el que tiene toda la información que nosotros deseemos ejecutar
+		return http.build();
+	}
 }
