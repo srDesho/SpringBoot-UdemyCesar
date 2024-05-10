c1c9d214d4ceab4e39720343faf9a4e640e93fd0
Cristian Montaño Laime <cristianlai775@gmail.com>
2024-05-09 22:29:48 -0400

    Creacion de Poyecto para API REST

---
+++ Curso_4_apis/src/main/java/com/cristianml/Curso4ApisApplication.java
@@ -0,0 +1,13 @@
+package com.cristianml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Curso4ApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Curso4ApisApplication.class, args);
+	}
+
+}
