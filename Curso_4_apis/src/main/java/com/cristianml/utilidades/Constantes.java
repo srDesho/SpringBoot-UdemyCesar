193f9f055035b4888a0b18afd2ddc980da045a1d
Cristian Montaño Laime <cristianlai775@gmail.com>
2024-05-09 23:07:31 -0400

    Clases constantes

---
+++ Curso_4_apis/src/main/java/com/cristianml/utilidades/Constantes.java
@@ -0,0 +1,11 @@
+package com.cristianml.utilidades;
+
+public class Constantes {
+	
+	// Ruta upload de nuestro servidor
+	public static final String RUTA_UPLOAD = "cristian.valores.ruta_upload=D:/Backend2024/StsProject/SpringBoot-UdemyCesar/upload/";
+	
+	// Firma para usarlo con el JWT, cuando se construye JWT siempre debe ser firmado, es como una contraseña segura.
+	public static final String FIRMA = "123456";
+
+}
