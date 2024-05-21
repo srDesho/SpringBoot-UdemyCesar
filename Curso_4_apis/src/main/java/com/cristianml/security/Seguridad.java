77e23eb1564047f3119bbf26f50c0eb90e691f06
Cristian Montaño Laime <cristianlai775@gmail.com>
2024-05-20 20:45:11 -0400

    Creando metodo SecurityFilterChain, dehabilitar CSRF

--- Curso_4_apis/src/main/java/com/cristianml/security/Seguridad.java
+++ Curso_4_apis/src/main/java/com/cristianml/security/Seguridad.java
@@ -5,8 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -25,4 +28,19 @@ public class Seguridad {
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
+	
+	// Creamos método para configurar el SecurityFilterChain
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		// Deshabilitamos el csrf
+		http.csrf().disable();
+		// Para que acepte todas las peticiones
+		http.authorizeRequests().anyRequest().permitAll();
+		// Decimos que va a trabajar con una política de creación estándar
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		return http.build(); 
+	}
+	
+	
+	
 }
